ca4084df96633569b56003ece1216c48c9e605b1
Zoe Nguyen <nganguyen0703@gmail.com>
2024-07-16 16:36:24 +0800

    feat: move sign out button to settings page

--- app/(tabs)/settings.jsx
+++ app/(tabs)/settings.jsx
@@ -1,11 +1,28 @@
 import { View, Text, Switch } from "react-native";
 import React from "react";
+import { router } from "expo-router";
 
 import { useThemeContext } from "../../context/ThemeProvider";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CustomButton from "../../components/CustomButton/CustomButton";
+import { useAuthContext } from "../../context/AuthProvider";
 
 const Settings = () => {
   const { theme, toggleTheme } = useThemeContext();
+  const { signOut, auth, isLoggedIn, isLoading } = useAuthContext();
+
+  if (!isLoading && !isLoggedIn) {
+    return <Redirect href="/" />
+  }
+
+  const submit = async () => {
+    try {
+      await signOut();
+      router.replace("/");
+    } catch (error) {
+      GlobalErrorHandler(error);
+    }
+  }
 
   return (
     <SafeAreaView className="bg-light-background dark:bg-dark-background h-full">
@@ -13,6 +30,17 @@ const Settings = () => {
         Toggle Dark Mode
       </Text>
       <Switch value={theme === "dark"} onValueChange={toggleTheme}/>
+      <View>
+        <CustomButton 
+          title="Sign Out"
+          handlePress={submit}
+        />
+        <CustomButton 
+          title="Back to index"
+          variant="secondary"
+          handlePress={() => router.push("/")}
+        />
+      </View>
     </SafeAreaView>
   )
 }
