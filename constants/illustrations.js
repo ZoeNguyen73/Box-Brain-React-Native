09fc8047eafdb6569432196b9eae302271426f06
Zoe Nguyen <nganguyen0703@gmail.com>
2024-07-23 20:17:41 +0800

    feat: update styling for sign-in page

--- constants/illustrations.js
+++ constants/illustrations.js
@@ -1,7 +1,27 @@
 import openDoodlesMessy from "../assets/illustrations/Open-Doodles-Messy.png"
 import openDoodlesStudying from "../assets/illustrations/Open-Doodles-Studying.png"
+import openDoodlesCoffee from "../assets/illustrations/Open-Doodles-Coffee.png"
+import openDoodlesDancing from "../assets/illustrations/Open-Doodles-Dancing.png"
+import openDoodlesFloat from "../assets/illustrations/Open-Doodles-Float.png"
+import openDoodlesGroovy from "../assets/illustrations/Open-Doodles-Groovy.png"
+import openDoodlesJumping from "../assets/illustrations/Open-Doodles-Jumping.png"
+import openDoodlesPlaying from "../assets/illustrations/Open-Doodles-Playing.png"
+import openDoodlesRunning from "../assets/illustrations/Open-Doodles-Running.png"
+import openDoodlesStrolling from "../assets/illustrations/Open-Doodles-Strolling.png"
+import openDoodlesSwinging from "../assets/illustrations/Open-Doodles-Swinging.png"
+import openDoodlesUnboxing from "../assets/illustrations/Open-Doodles-Unboxing.png"
 
 export default {
   openDoodlesMessy,
-  openDoodlesStudying
+  openDoodlesStudying,
+  openDoodlesCoffee,
+  openDoodlesDancing,
+  openDoodlesFloat,
+  openDoodlesGroovy,
+  openDoodlesJumping,
+  openDoodlesPlaying,
+  openDoodlesRunning,
+  openDoodlesStrolling,
+  openDoodlesSwinging,
+  openDoodlesUnboxing,
 };
