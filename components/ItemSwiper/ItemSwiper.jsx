bc4d1659083c175e717b551f5a734f70ce8f15c2
Zoe Nguyen <nganguyen0703@gmail.com>
2024-07-24 14:32:07 +0800

    feat: add finish page after user has reviewed all cards

--- components/ItemSwiper/ItemSwiper.jsx
+++ components/ItemSwiper/ItemSwiper.jsx
@@ -1,16 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React, {useState, useEffect} from "react";
 import Swiper from "react-native-deck-swiper";
+import { router } from "expo-router";
 
 import sampleData from "./sampleData.json";
 import ItemCard from "../ItemCard/ItemCard";
 import ColorPairings from "./ColorPairings";
 import ProgressBar from "../ProgressBar";
+import CustomButton from "../CustomButton/CustomButton";
+
+import { illustrations } from "../../constants";
 
 const ItemSwiper = () => {
   const [items, setItems] = useState([]);
   const [counter, setCounter] = useState(0);
   const [amount, setAmount] = useState("0%");
+  const [swipedAllCards, setSwipedAllCards] = useState(false);
   
   useEffect(() => {
     // assign colors to each item
@@ -57,7 +62,7 @@ const ItemSwiper = () => {
       colorIndex = Math.floor(Math.random() * ColorPairings.length);
     }
     return colorIndex;
-  }
+  };
 
   return (
     <View>
@@ -85,26 +90,59 @@ const ItemSwiper = () => {
           </View>
 
           <View>
-            <Swiper
-              cards={items}
-              keyExtractor={(item) => item._id}
-              cardIndex={0}
-              renderCard={(item, index) => (
-                  <ItemCard 
-                    item={item}
-                    index={index}
+            { !swipedAllCards && (
+              <Swiper
+                cards={items}
+                keyExtractor={(item) => item._id}
+                cardIndex={0}
+                renderCard={(item, index) => (
+                    <ItemCard 
+                      item={item}
+                      index={index}
+                    />
+                  )
+                }
+                stackSize={3}
+                stackSeparation={20}
+                animateCardOpacity
+                onSwipedLeft={() => swipeAction("left")}
+                onSwipedRight={() => swipeAction("right")}
+                verticalSwipe={false}
+                cardVerticalMargin={30}
+                cardHorizontalMargin={30}
+                onSwipedAll={() => setSwipedAllCards(true)}
+              />
+            )}
+
+            { swipedAllCards && (
+              <View className="flex justify-center items-center h-[80vh]">
+                <View className="bg-light-yellow w-[250px] h-[250px] rounded-full justify-center items-center mb-10">
+                  <Image
+                    source={illustrations.openDoodlesSwinging}
+                    style={{ maxWidth: "100%" }}
+                    resizeMode="contain"
                   />
-                )
-              }
-              stackSize={3}
-              stackSeparation={20}
-              animateCardOpacity
-              onSwipedLeft={() => swipeAction("left")}
-              onSwipedRight={() => swipeAction("right")}
-              verticalSwipe={false}
-              cardVerticalMargin={30}
-              cardHorizontalMargin={30}
-            />
+                </View>
+                
+                <View className="w-[80%] justify-center items-center">
+                  <Text className="font-serif-bold text-4xl tracking-wider text-light-text dark:text-light-yellow">
+                    Great job!
+                  </Text>
+                  <Text className="font-sans tracking-wide text-light-text dark:text-dark-text mt-1">
+                    You have completed all items today
+                  </Text>
+                </View>
+
+                <CustomButton
+                  containerStyles="mt-20" 
+                  title="Back to Home"
+                  handlePress={() => router.push("/home")}
+                />
+               
+              </View>
+              
+            )}
+            
           </View>
           
         </View>
